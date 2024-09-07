bc0e9dea7625ae20c56aa44b54c9bd1acd406eb4
Mariwan <sehadmarewan@gmail.com>
2024-09-07 13:04:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Banner from "./components/Banner/Banner"
+import Banner2 from "./components/Banner/Banner2"
+import Brands from "./components/Brands/Brands"
+import Hero from "./components/Hero/Hero"
+import Navbar from "./components/Navbar/Navbar"
+import Services from "./components/Services/Services"
+import Testimonial from "./components/Testimonial/Testimonial"
+import Newsletter from "./components/Newsletter/Newsletter";
+import Footer from "./components/Footer/Footer"
+
+function App() {
+
+  return (
+    <main className='overflow-x-hidden'>
+        <Navbar />
+        <Hero />
+        <Brands />
+        <Services />
+        <Banner />
+        <Banner2 />
+        <Testimonial />
+        <Newsletter />
+        <Footer />
+    </main>
+  )
+}
+
+export default App
